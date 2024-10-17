316787ddb632bfba1b4c682d1291e69380b5fa78
Maksim Skomorokhov <nullnvrsk@gmail.com>
2024-10-17 14:18:28 +0300

    remove unnecessary imports

--- src/test/java/github/WikiPageTests.java
+++ src/test/java/github/WikiPageTests.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withTagAndText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class WikiPageTests {
